31795abfae26285529fdd9a47d67a6e276095f05
Komarka <robocodeuserpro@gmail.com>
2025-06-07 15:50:27 +0300

    enemy and UI

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,17 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class PlayerMovement : MonoBehaviour
 {
     public float moveH, moveV;
     public float moveSpeed = 1.0f;
     private Rigidbody2D rb;
-    
+    public Text CoinTxt;
+    public Text HpTxt;
+    public float HP;
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+
     }
     private void Awake()
     {
@@ -19,6 +24,11 @@ public class PlayerMovement : MonoBehaviour
     }
     private void FixedUpdate()
     {
+        if(HP <= 0)
+        {
+            SceneManager.LoadScene(0);
+        }
+        HpTxt.text = HP.ToString();
         moveH = Input.GetAxis("Horizontal") * moveSpeed;
         moveV = Input.GetAxis("Vertical") * moveSpeed;
         rb.velocity = new Vector2(moveH, moveV);

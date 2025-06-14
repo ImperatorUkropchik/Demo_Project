4f9cc63605f6257ff5c937f451765ea36eba55d1
Komarka <bhyuty14@gmail.com>
2025-06-14 20:04:08 +0300

    Added animation at the start of level

--- Assets/Scripts/Anima.cs
+++ Assets/Scripts/Anima.cs
@@ -5,36 +5,67 @@ using UnityEngine.UI;
 
 public class Anima : MonoBehaviour
 {
-    private bool TimeForTema = false;
     public Image tema;
-    private float temka = 255f;
+    private float temka = 1f;
+    public GameObject player;
+    public GameObject camira;
+    private bool nado = true;
+    private bool nnado = true;
     // Start is called before the first frame update
     void Start()
     {
-        Invoke("PereZatem", 0.5f);
+        //Invoke("PereZatem", 0.5f);
+        StartCoroutine(Zatema());
+        //camira.transform.position = new Vector3(20f, player.transform.position.y, -10f);
     }
-    IEnumerator Zatema()
+    private IEnumerator Zatema()
     {
-        while (TimeForTema)
+        while (nado == true)
         {
             tema.color = new Color(0f, 0f, 0f, temka);
-            temka -= 1f;
-            if(temka >= 1f)
+            temka -= 0.025f;
+            if(temka <= 0f)
             {
-                TimeForTema = false;
+                StopCoroutine(Zatema());
+                nado = false;
+                Debug.Log(tema.color.a);
+                Invoke("CamAn", 1f);
+
             }
-            yield return new WaitForSeconds(0.1f);
+
+            yield return new WaitForSeconds(0.05f);
+        }
+    }
+    private IEnumerator CamAnima()
+    {
+        float x;
+        x = camira.transform.position.x;
+        while (nnado == true)
+        {
+            Debug.Log(tema.color.a);
+            camira.transform.position = new Vector3(x, player.transform.position.y, -10f);
+            x -= 0.1f;
+            if(x <= player.transform.position.x)
+            {
+                StopCoroutine(CamAnima());
+                nnado = false;
+            }
+            yield return new WaitForSeconds(0.01f);
         }
 
     }
+    void CamAn()
+    {
+        StartCoroutine(CamAnima());
+    }
     void PereZatem()
     {
-        TimeForTema = true;
-        StartCoroutine(Zatema());
+        //TimeForTema = true;
+        //StartCoroutine(Zatema());
     }
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
